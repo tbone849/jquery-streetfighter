2af9c6bbe642122d376143502e95bf340775d484
Tim Sarvey <timsarvey@gmail.com>
2015-09-11 14:49:49 -0400

    Add Ryu 'cool' feature

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function(){
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show().animate(
@@ -23,3 +24,25 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
 	});
 });
+
+$(document).keydown(function(e){
+	if (e.keyCode==88){
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-cool').show();
+	}
+});
+
+$(document).keyup(function(e){
+	if (e.keyCode==88){
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+	}
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
